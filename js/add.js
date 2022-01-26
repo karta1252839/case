e43bff73b417384eddd0093fd2e205e6b227b0c0
DESKTOP-BLLKHOG\user <karta1252839@yahoo.com.tw>
2022-01-26 16:57:36 +0800

    2

---
+++ js/add.js
@@ -0,0 +1,6 @@
+// 版權
+function PageHeader() {
+    document.writeln("<div class=\"col-12 bg-secondary\">");
+    document.writeln("            <p class=\"py-2 text-light text-center\">@所有版權未經本人合法授權任意複制版權必究</p>");
+    document.writeln("        </div>");
+}
